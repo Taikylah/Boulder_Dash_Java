2aa549953d089e96f6498b1a5142c2c192387412
Taikylah <25254922+Taikylah@users.noreply.github.com>
2019-05-30 15:25:08 +0200

    frame size fixed

--- view/src/main/java/view/ViewFrame.java
+++ view/src/main/java/view/ViewFrame.java
@@ -132,7 +132,7 @@ class ViewFrame extends JFrame implements KeyListener {
 		this.setResizable(false);
 		this.addKeyListener(this);
 		this.setContentPane(new ViewPanel(this));
-		this.setSize(320 + this.getInsets().left + this.getInsets().right, 352 + this.getInsets().top + this.getInsets().bottom);
+		this.setSize(325 + this.getInsets().left + this.getInsets().right, 349 + this.getInsets().top + this.getInsets().bottom);
 		this.setLocationRelativeTo(null);
 	}
 
